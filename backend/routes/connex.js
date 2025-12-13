9f057c83eefba7c5dacd5f5813e75b21386f3ab7
Nassera <nasseranabgaoui@gmail.com>
2025-12-13 09:23:51 +0100

    init

--- backend/routes/connex.js
+++ backend/routes/connex.js
@@ -1,8 +1,10 @@
 var bcrypt = require("bcrypt");
+// CORRECTION ICI : On pointe vers le vrai modèle User, pas vers ce fichier lui-même
 var User = require("../models/User"); 
 
 // LOGIN
 exports.login = async (req, res) => {
+    // Utilisation de var comme à la page 23 du cours pour la partie serveur
     var email = req.body.email;
     var motDePasse = req.body.motDePasse;
 
@@ -10,17 +12,19 @@ exports.login = async (req, res) => {
         return res.status(400).json({ message: "Champs manquants" });
     }
 
-    // Utilisation de var user
+    // Recherche de l'utilisateur
     var user = await User.findOne({ email: email });
     if (!user) {
         return res.status(400).json({ message: "Email ou mot de passe incorrect" });
     }
 
+    // Vérification du mot de passe
     var ok = await bcrypt.compare(motDePasse, user.motDePasse);
     if (!ok) {
         return res.status(400).json({ message: "Email ou mot de passe incorrect" });
     }
 
+    // On renvoie le token et les infos
     res.json({
         message: "Bienvenue",
         token: "TOKEN-NEXUSGO",
@@ -30,7 +34,6 @@ exports.login = async (req, res) => {
         }
     });
 };
-
 
 
 
