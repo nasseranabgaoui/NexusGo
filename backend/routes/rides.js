5df53f180a8cc9ba4c3b3e57d6438b26c2c0f9ff
Nassera <nasseranabgaoui@gmail.com>
2025-12-16 21:01:59 +0100

    final

--- backend/routes/rides.js
+++ backend/routes/rides.js
@@ -2,17 +2,18 @@ var express = require("express");
 var router = express.Router();
 var Ride = require("../models/Ride");
 
+// Recherche 
 router.get("/", async (req, res) => {
     var query = {};
 
     if (req.query.villeDepart) {
-        query.villeDepart = req.query.villeDepart;
+        query.villeDepart = new RegExp(req.query.villeDepart, 'i');
     }
     if (req.query.villeArrivee) {
-        query.villeArrivee = req.query.villeArrivee;
+        query.villeArrivee = new RegExp(req.query.villeArrivee, 'i');
     }
     if (req.query.date) {
-        query.date = req.query.date;
+        query.date = Number(req.query.date); 
     }
 
     if (req.query.prixMax) {
@@ -22,13 +23,27 @@ router.get("/", async (req, res) => {
             query.prix = { $lte: maxPriceNumber }; 
         }
     }
+    
+    
     query.nbPlaces = { $gt: 0 };
 
     try {
         var rides = await Ride.find(query).sort({ date: 1 });
         res.json(rides);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ message: "Erreur serveur lors de la recherche" });
+    }
+});
+
+
+// Création 
+router.post("/", async function(req, res) {
+    try {
+        var newRide = new Ride(req.body);
+        var savedRide = await newRide.save();
+        res.status(201).json({ message: "Trajet créé", ride: savedRide });
+    } catch (err) {
+        res.status(400).json({ error: "Erreur lors de l'enregistrement du trajet (mauvaises données fournies)" });
     }
 });
 
